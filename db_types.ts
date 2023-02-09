4e872c71f3660d51e105f37844f45fd616b7066f
Julien Tanguy <tanguy.j@sfeir.com>
2023-02-09 09:35:55 +0100

    chore: regenerate types

--- db_types.ts
+++ db_types.ts
@@ -80,21 +80,18 @@ export interface Database {
           email: string | null
           full_name: string | null
           id: string
-          updated_at: string | null
         }
         Insert: {
           avatar_url?: string | null
           email?: string | null
           full_name?: string | null
           id: string
-          updated_at?: string | null
         }
         Update: {
           avatar_url?: string | null
           email?: string | null
           full_name?: string | null
           id?: string
-          updated_at?: string | null
         }
       }
     }
