2b2791bdcecf85753cf0ee51be61efe40a405c63
Julien Tanguy <tanguy.j@sfeir.com>
2023-03-08 11:24:15 +0100

    feat: use resource route for serving avatars

---
+++ app/routes/__supabase/__auth/avatars.tsx
@@ -0,0 +1,39 @@
+import { createElement } from "react";
+import { renderToString } from "react-dom/server";
+import type { LoaderArgs } from "@remix-run/node";
+
+import Avatar from "boring-avatars";
+
+const SIZE = 96;
+const VARIANT = "bauhaus";
+const COLORS = ["#1a2a3a", "#e4426d", "#1d1d2b", "#769cec", "#586f8f"];
+
+export async function loader({ request }: LoaderArgs) {
+  const url = new URL(request.url);
+
+  const explicitName = url.searchParams.get("name");
+  const name = explicitName || Math.random().toString();
+
+  const svg = renderToString(
+    createElement(
+      Avatar,
+      {
+        size: SIZE,
+        name,
+        variant: VARIANT,
+        colors: COLORS,
+        square: false,
+      },
+      null
+    )
+  );
+
+  return new Response(svg, {
+    headers: {
+      "Content-Type": "image/svg+xml",
+      "Cache-Control": explicitName
+        ? "max-age=0, s-maxage=2592000"
+        : "max-age=0, s-maxage=1",
+    },
+  });
+}
