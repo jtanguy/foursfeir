a10f0ed9ebecb5c88ab37153eb6a30b9451cf383
Julien Tanguy <tanguy.j@sfeir.com>
2023-03-08 14:40:21 +0100

    feat: add loader on other form submit

--- app/routes/__supabase/__auth/$city/$date.tsx
+++ app/routes/__supabase/__auth/$city/$date.tsx
@@ -1,11 +1,15 @@
-import {useState} from "react"
+import { useState } from "react";
 import { Temporal } from "@js-temporal/polyfill";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, useLoaderData, useParams } from "@remix-run/react";
+import {
+  Form,
+  useLoaderData,
+  useParams,
+  useTransition,
+} from "@remix-run/react";
 import { createServerClient } from "utils/supabase.server";
 import Avatar from "~/components/Avatar";
-
 
 export const loader = async ({ request, params }: LoaderArgs) => {
   const response = new Response();
@@ -15,22 +19,24 @@ export const loader = async ({ request, params }: LoaderArgs) => {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const [{ data: bookings }, { data: cities }] = await Promise.all([
+    supabase
+      .from("bookings")
+      .select("period, profiles:user_id(id, email, full_name, avatar_url)")
+      .eq("city", params.city)
+      .eq("date", params.date),
+    supabase.from("cities").select("capacity").eq("slug", params.city),
+  ]);
 
-  const [{ data: bookings }, { data: cities }] =
-    await Promise.all([
-      supabase
-        .from("bookings")
-        .select("period, profiles:user_id(id, email, full_name, avatar_url)")
-        .eq("city", params.city)
-        .eq("date", params.date),
-      supabase.from("cities").select("capacity").eq("slug", params.city)
-    ]);
+  const excludedIds: string[] = [
+    user!.id,
+    ...(bookings ?? []).map((b) => b!.profiles!.id),
+  ];
 
-    const excludedIds: string[] = [user!.id, ...(bookings ?? []).map(b => b!.profiles!.id)]
-
-    const {data: profiles} = await supabase.from("profiles")
+  const { data: profiles } = await supabase
+    .from("profiles")
     .select("id, email, full_name")
-    .not('id', 'in', `(${excludedIds.join(',')})`)
+    .not("id", "in", `(${excludedIds.join(",")})`);
 
   return json({
     bookings: bookings ?? [],
@@ -43,6 +49,7 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 export default function Current() {
   const { city, date: dateStr } = useParams();
   const { bookings, capacity, profiles } = useLoaderData<typeof loader>();
+  const transition = useTransition();
 
   const [showNameInput, setShowNameInput] = useState(false);
 
@@ -56,17 +63,18 @@ export default function Current() {
   const handleChange = (event) => {
     const emailStr = event.target.value.trim();
     const isEmailLike = emailStr.includes("@");
-    const profileExists = profiles.some(p => p.email.startsWith(emailStr)) || bookings.some(b => b.profiles.email.startsWith(emailStr));
+    const profileExists =
+      profiles.some((p) => p.email.startsWith(emailStr)) ||
+      bookings.some((b) => b.profiles.email.startsWith(emailStr));
 
-    if(!isEmailLike && showNameInput){
-      setShowNameInput(false)
+    if (!isEmailLike && showNameInput) {
+      setShowNameInput(false);
     }
 
-    if(isEmailLike && !showNameInput && !profileExists){
-      setShowNameInput(true)
+    if (isEmailLike && !showNameInput && !profileExists) {
+      setShowNameInput(true);
     }
-  }
-
+  };
 
   return (
     <>
@@ -85,7 +93,7 @@ export default function Current() {
       <ul className="calendar-people__list">
         {bookings.map((booking) => (
           <li key={booking.profiles?.id}>
-            <Avatar profile={booking.profiles} size={96}/>
+            <Avatar profile={booking.profiles} size={96} />
             <span>
               {booking.profiles?.full_name ?? booking.profiles.email}{" "}
               {periods[booking.period] && <> ({periods[booking.period]})</>}
@@ -98,7 +106,12 @@ export default function Current() {
         <input type="hidden" name="date" value={dateStr} />
         <label>
           Email
-          <input type="email" name="for_user" list="other" onChange={handleChange} />
+          <input
+            type="email"
+            name="for_user"
+            list="other"
+            onChange={handleChange}
+          />
         </label>
         <datalist id="other">
           {profiles.map((p) => {
@@ -109,28 +122,32 @@ export default function Current() {
             );
           })}
         </datalist>
-        {showNameInput && <label>
-          Nom
-          <input type="text" name="for_user_name" />
-        </label>}
+        {showNameInput && (
+          <label>
+            Nom
+            <input type="text" name="for_user_name" />
+          </label>
+        )}
         <fieldset>
           <legend>Période</legend>
           <label>
-            <input type="radio" name="period" value="day" defaultChecked/>
+            <input type="radio" name="period" value="day" defaultChecked />
             Journée
           </label>
           <label>
-            <input type="radio" name="period" value="morning"/>
+            <input type="radio" name="period" value="morning" />
             Matin
           </label>
           <label>
-            <input type="radio" name="period" value="afternoon"/>
+            <input type="radio" name="period" value="afternoon" />
             Après-midi
           </label>
         </fieldset>
 
         <input type="hidden" name="action" value="book" />
-        <input type="submit" value="Inscrire"/>
+        <button type="submit" aria-busy={transition.state === "submitting"}>
+          Inscrire
+        </button>
       </Form>
     </>
   );
