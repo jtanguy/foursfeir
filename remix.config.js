1e247a4ae22e00f4c432f663d1f6dba928d05231
Julien Tanguy <tanguy.j@sfeir.com>
2023-08-24 20:19:39 +0200

    chore: v2 dev server

--- remix.config.js
+++ remix.config.js
@@ -15,11 +15,13 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
+  serverDependenciesToBundle: ["@js-temporal/polyfill"],
   future: {
     v2_routeConvention: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
     v2_meta: true,
     v2_headers: true,
+    v2_dev: true,
   },
 };
