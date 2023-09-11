f50cb1acea4f41522e56760a3cbf9c0816e97813
Julien Tanguy <julien.tanguy@jhome.fr>
2023-09-11 16:05:57 +0200

    fix: import temporal

--- app/routes/_auth.$city.today.tsx
+++ app/routes/_auth.$city.today.tsx
@@ -1,3 +1,4 @@
+import { Temporal } from "@js-temporal/polyfill";
 import { redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/node";
@@ -5,8 +6,4 @@ import type { LoaderArgs } from "@remix-run/node";
 export const loader = async ({request}: LoaderArgs) => {
     const today = Temporal.Now.plainDateISO();
     return redirect(`${request.params.city}/${today.toString()}`, 302)
-}
-
-export default function Layout() {
-  return <Outlet />;
 }
