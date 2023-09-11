51241c6e7cbac1a29c769502b05c5ae977aa4a76
Julien Tanguy <julien.tanguy@jhome.fr>
2023-09-11 15:58:56 +0200

    fix: throw redirect

--- app/routes/_auth.$city.today.tsx
+++ app/routes/_auth.$city.today.tsx
@@ -3,5 +3,5 @@ import type { LoaderArgs } from "@remix-run/node";
 
 export const loader = async ({request}: LoaderArgs) => {
     const today = Temporal.Now.plainDateISO();
-    return redirect(`${request.params.city}/${today.toString()}`, 302)
+    throw redirect(`${request.params.city}/${today.toString()}`, 302)
 }
