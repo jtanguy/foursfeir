bea23029f2f1ee734dc678d2d4b02effa7ea8c7e
Julien Tanguy <julien.tanguy@jhome.fr>
2023-09-11 15:54:59 +0200

    fix: make today compile

--- app/routes/_auth.$city.today.tsx
+++ app/routes/_auth.$city.today.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "@remix-run/node";
 import type { LoaderArgs } from "@remix-run/node";
 
-export const loader: async ({request}: LoaderArgs) => {
+export const loader = async ({request}: LoaderArgs) => {
     const today = Temporal.Now.plainDateISO();
     return redirect(`${request.params.city}/${today.toString()}`, 302)
 }
