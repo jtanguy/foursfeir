d928fc8c4b04be835f242016c6746e56901f11a7
Julien Tanguy <tanguy.j@sfeir.com>
2025-02-24 16:19:43 +0100

    fix: mirror creds on vercel

--- app/services/db/client.server.ts
+++ app/services/db/client.server.ts
@@ -1,6 +1,13 @@
 import { Datastore } from "@google-cloud/datastore";
+import { env } from "config";
 
-export const client = new Datastore()
+export const client = new Datastore({
+	credentials: {
+		client_email: env.GCP_SERVICE_ACCOUNT_EMAIL,
+		private_key: env.GCP_PRIVATE_KEY,
+	},
+	projectId: env.GCP_PROJECT_ID,
+})
 
 export const KINDS = {
 	profile: 'Profile',
--- config.ts
+++ config.ts
@@ -3,7 +3,12 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    // GOOGLE_APPLICATION_CREDENTIALS: z.string(),
+    // For Datastore
+    GCP_SERVICE_ACCOUNT_EMAIL: z.string(),
+    GCP_PRIVATE_KEY: z.string(),
+    GCP_PROJECT_ID: z.string(),
+
+    // Oauth
     GOOGLE_AUTH_ID: z.string(),
     GOOGLE_AUTH_SECRET: z.string(),
     GOOGLE_AUTH_CALLBACK_URL: z.string().url(),
