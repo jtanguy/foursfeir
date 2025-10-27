6a2821b8572582682ce505c79a4db4e6e6e389ee
Julien Tanguy <julien.tanguy@jhome.fr>
2025-10-27 21:50:37 +0100

    feat: user profile sync

--- app/routes/_auth.me.tsx
+++ app/routes/_auth.me.tsx
@@ -1,9 +1,13 @@
-import { json, MetaFunction, type LoaderFunctionArgs } from "@remix-run/node";
+import { ActionFunctionArgs, json, MetaFunction, type LoaderFunctionArgs } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { Temporal } from "temporal-polyfill";
 import { getUserFromRequest } from "~/services/auth.server";
-import { bookingService } from "~/services/application/services.server";
+import { bookingService, profileService } from "~/services/application/services.server";
 import { cityService } from "~/services/application/services.server";
+import Avatar from "~/components/Avatar";
+import { FaSync } from "react-icons/fa";
+import { zfd } from "zod-form-data";
+import z from "zod";
 
 export const meta: MetaFunction = () => [
   { title: "FourSFEIR | Mes réservations" },
@@ -21,14 +25,37 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     month = Temporal.Now.plainDateISO().toPlainYearMonth();
   }
 
+  const profile = await profileService.getProfileById(user.user_id);
   const cities = await cityService.getCities();
   const bookings = await bookingService.getMyBookings(user.user_id, month);
 
-  return json({ bookings, cities, date: month.toString() });
+  return json({ bookings, cities, date: month.toString(), profile, user });
+};
+
+const actionSchema = zfd.formData(
+  z.discriminatedUnion("_action", [
+    z.object({
+      _action: z.literal("update-avatar"),
+      full_name: zfd.text(z.string()),
+      avatar_url: zfd.text(z.string().url())
+    }),
+  ]),
+);
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const user = await getUserFromRequest(request);
+  const f = actionSchema.parse(await request.formData());
+  const profile = await profileService.getProfileById(user.user_id);
+
+  if (f._action === "update-avatar") {
+    const newProfile = { ...profile, user_id: user.user_id, full_name: f.full_name, avatar_url: f.avatar_url };
+    await profileService.updateProfile(newProfile);
+  }
+
+  return new Response(null, { status: 204 });
 };
 
 export default function MeRoute() {
-  const { bookings, cities, date: month } = useLoaderData<typeof loader>();
+  const { bookings, cities, date: month, profile, user } = useLoaderData<typeof loader>();
   const currentMonth = Temporal.PlainYearMonth.from(month);
   const prevMonth = currentMonth.subtract({ months: 1 });
   const nextMonth = currentMonth.add({ months: 1 });
@@ -105,6 +132,33 @@ export default function MeRoute() {
           </button>
         </div>
       </Form>
+
+      <h2>Profil</h2>
+
+      <div className="grid">
+        <article>
+          <header>Session Google</header>
+          <div className="grid">
+            <Avatar className="avatar--huge" profile={user} />
+            <p className="center-content">{user.full_name}</p>
+          </div>
+        </article>
+        <Form className="center-content" method="post" action="/me">
+          <input type="hidden" name="_action" value="update-avatar" />
+          <input type="hidden" name="full_name" value={user.full_name} />
+          <input type="hidden" name="avatar_url" value={user.avatar_url} />
+          <button type="submit"><FaSync /> Synchroniser</button>
+        </Form>
+        {profile && (
+          <article>
+            <header>Profil Foursfeir</header>
+            <div className="grid">
+              <Avatar className="avatar--huge" profile={profile} />
+              <p className="center-content">{profile.full_name}</p>
+            </div>
+          </article>
+        )}
+      </div>
     </main>
   );
 }
